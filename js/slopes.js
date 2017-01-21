fae1cad22e490cb81ae3bdef3cdc0aa2c3aac17e
Dom Wakeling <domwakeling@gmail.com>
2017-01-21 13:18:45 +0000

    Change URLs to GitHub

--- js/slopes.js
+++ js/slopes.js
@@ -36,8 +36,8 @@ var div = d3.select("body").append("div")
     .style("opacity", 0);
 
 // URLs for [1] a 110-m map stored in a GitHub repo and [2] the slopes data in a GitHub repo
-var url1 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/world.json";
-var url2 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/skislopes.json";
+var url1 = "https://raw.githubusercontent.com/domwakeling/uk_skislopes/master/data/world.json";
+var url2 = "https://raw.githubusercontent.com/domwakeling/uk_skislopes/master/data/skislopes.json";
 
 d3.queue(2)
     .defer(d3.json, url1)
